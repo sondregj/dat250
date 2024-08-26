459b7b35cd0fd2c434c140973309680e1593a404
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2024-08-26 10:55:49 +0200

    add home route

--- src/main/java/no/hvl/student/_1/dat250/exercise1/Exercise1Application.java
+++ src/main/java/no/hvl/student/_1/dat250/exercise1/Exercise1Application.java
@@ -2,12 +2,20 @@ package no.hvl.student._1.dat250.exercise1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Exercise1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Exercise1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Exercise1Application.class, args);
+    }
 
+    @GetMapping("/")
+    public String home() {
+        return "<h1>Exercise 1</h1>";
+    }
 }
