e80c4ed273249867c786a6363aebb09d9b26db59
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2024-09-01 23:28:21 +0200

    remove unused import

--- src/main/java/no/hvl/student/_1/dat250/exercise1/Exercise1Application.java
+++ src/main/java/no/hvl/student/_1/dat250/exercise1/Exercise1Application.java
@@ -3,7 +3,6 @@ package no.hvl.student._1.dat250.exercise1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
