9166a52e7fb75bda3592c59c9d9b916d62e2fbc3
Sondre Gjellestad <sondre.gjellestad@icloud.com>
2024-09-09 10:07:16 +0200

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    implementation("com.google.guava:guava:32.1.1-jre")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
